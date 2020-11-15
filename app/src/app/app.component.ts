a4f0562e11f8092ad9dab68aa5f1271cf0f87df5
Samuel da Silva Sirkis <37945986+samuelsirkis@users.noreply.github.com>
2020-11-15 00:53:20 -0300

    chore: Add libs and first configurations.

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Samuel';
+  name = 'Samuel';
+  email = 'samuel@email.com';
 }
